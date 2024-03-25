e840855ca07c4604164b27733f36675a46a8247e
Kiss Cintia <cintia.525.kiss@gmail.com>
2024-03-25 14:35:01 +0100

    Add project files.

---
+++ Haromszog/HaromszogAdatai.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Haromszog
+{
+    public class HaromszogAdatai
+    {
+        public double SideA;
+        public double SideB;
+        public double SideC;
+
+        public HaromszogAdatai(double SideA, double SideB, double SideC)
+        {
+            this.SideA = SideA;
+            this.SideB = SideB;
+            this.SideC = SideC;
+        }
+
+        public double Kerulet()
+        {
+            return (SideA + SideB + SideC);
+        }
+        public double Terulet()
+        {
+            double s = (SideA + SideB + SideC) / 2;
+            return Math.Sqrt(s * (s - SideA) * (s - SideB) * (s - SideC));
+        }
+    }
+}
---
+++ Haromszog/Program.cs
@@ -0,0 +1,6 @@
+﻿using Haromszog;
+
+HaromszogAdatai Haromszog = new HaromszogAdatai(12, 23, 34);
+
+Console.WriteLine($"Háromszög területe: {Haromszog.Terulet()}");
+Console.WriteLine($"Háromszög kerülete: {Haromszog.Kerulet()}");
